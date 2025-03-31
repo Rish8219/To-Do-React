13f474fce930a1bc41660e9073024d635e2c3434
Rishabh Sharma <rishabhsharma.7018@gmail.com>
2025-03-31 17:56:39 +0530

    done some visual changes

--- To-do/src/components/todo.jsx
+++ To-do/src/components/todo.jsx
@@ -19,8 +19,8 @@ const Todo = () => {
         else {
             setList([...list, data.trim()])
             //validation for whiteSpace only 
-                setData("");
-           
+            setData("");
+
 
             //setting in local storage keeping first item also
             localStorage.setItem('list', JSON.stringify([...list, data]))
@@ -92,8 +92,7 @@ const Todo = () => {
                     <h2 className='text-4xl text-white mt-5 ml-5 '>👋Getting Started</h2>
                     <div className='h-auto w-auto' >
                         <input type="text" placeholder="Enter your todo......" ref={inputBox} value={data} onChange={(e) => setData(e.target.value)} className="bg-black p-2 mt-10 ml-9 w-11/14 h-10 shadow-black shadow-2xl text-white rounded-2xl border-none outline-none placeholder:text-gray-100 focus:ring-2 focus:ring-green-600" />
-                        <button className="bg-yellow-400 text-white ml-4 p-2 mt-8 rounded-2xl border-none outline-none hover:bg-yellow-500 " onClick={handleBtn} style={{ display: isClicked ? "none" : "inline" }}>Add-To</button>  <button className="bg-indigo-400 text-white ml-3 h-10 w-20 mt-8 rounded-2xl border-none outline-none hover:bg-indigo-500 " onClick={() => handleUpdate()}>Update</button> :
-                        <div className=' content-div mt-8 flex-col justify-self-start w-11/12'>
+                        <button className="bg-yellow-400 text-white ml-4 p-2 mt-8 rounded-2xl border-none outline-none hover:bg-yellow-500 " onClick={handleBtn} style={{ display: isClicked ? "none" : "inline" }}>Add-To</button> 
                             <h1 style={{ display: list.length ? "block" : "none" }} className='text-4xl text-center text-white italic'>TO-DO LIST </h1>
                             {list.map((item, index) => {
                                 return (
