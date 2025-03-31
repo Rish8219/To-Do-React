e5bb0fcbe1cd5a3dfa0ab8b61e4a0457205ddbc1
Rishabh Sharma <rishabhsharma.7018@gmail.com>
2025-03-31 17:09:55 +0530

    added some validation for empty input fields

--- To-do/src/components/list.jsx
+++ To-do/src/components/list.jsx
@@ -5,12 +5,8 @@ const List = ({ list, item, handleDelete, index, handleEdit, handleUpdate, isCli
     const handleComplete = () => {
         setShow(!show)
         console.log(list);
-
     }
-
     return (
-
-
         <div className='flex'>
             <p type="text" style={{ textDecoration: show ? "none" : "line-through", backgroundColor: show ? "white" : "black", transition: "all 2s", color: show ? "black" : "white" }} className=" px-3 p-0.5 mt-8 ml-9 text-2xl w-11/16 h-auto shadow-black shadow-2xl text-black rounded-2xl border-none outline-none">{item}</p>
             <button className=" mt-8 ml-3 rounded-2xl h-10 w-20 text-white bg-red-400 hover:bg-red-700" style={{ display: isClicked ? "none" : "inline" }} onClick={() => handleDelete(index)}>Delete</button>
@@ -18,10 +14,6 @@ const List = ({ list, item, handleDelete, index, handleEdit, handleUpdate, isCli
             {
                 isClicked ? <button className="bg-indigo-400 text-white ml-3 h-10 w-20 mt-8 rounded-2xl border-none outline-none hover:bg-indigo-500 " onClick={() => handleUpdate(index)}>Update</button> : <button className=" mt-8 ml-3 rounded-2xl h-10 w-20 text-white bg-green-400 hover:bg-green-700" onClick={() => handleEdit(item, index)} >Edit</button>
             }
-
-
-
-
 
         </div>
 
--- To-do/src/components/todo.jsx
+++ To-do/src/components/todo.jsx
@@ -7,15 +7,35 @@ const Todo = () => {
     const [isClicked, setIsClicked] = useState(false)
 
     let inputBox = useRef(null)
+
     const handleBtn = () => {
-        if (data !== "") {
-            setList([...list, data])
-        //setting in local storage keeping first item also
-        localStorage.setItem('list', JSON.stringify([...list, data]))
-        // localStorage.setItem('list', JSON.stringify(list))
+
+
+
+        if (data.trim() == "") {
             setData("")
 
         }
+        else {
+            setList([...list, data.trim()])
+            //validation for whiteSpace only 
+            if (data.trim() !== "") {
+                setData("");
+            }
+
+            //setting in local storage keeping first item also
+            localStorage.setItem('list', JSON.stringify([...list, data]))
+            setData("")
+        }
+        //validation so that empty string should not get stored in list
+
+
+
+
+
+
+
+
     }
     const handleDelete = (key) => {
         let newList = [...list]
@@ -25,27 +45,32 @@ const Todo = () => {
         localStorage.setItem('list', JSON.stringify(newList))
     }
 
-    const handleEdit = (item) => {
+
+    const handleEdit = (item,) => {
         setIsClicked(true)
         inputBox.current.focus()
         setData(item)
+
+
 
     }
 
     const handleUpdate = (index) => {
         setIsClicked(false)
-        if(data){
+        if (data) {
             let newList = [...list]
             newList[index] = data
             setList(newList)
             //update functionality in local storage
             localStorage.setItem('list', JSON.stringify(newList))
             setData("")
+            console.log(index);
         }
-       else{
-        alert("Cant be empty")      
+        else {
+            alert("Cant be empty")
 
-       }
+        }
+
 
 
     }
@@ -54,25 +79,29 @@ const Todo = () => {
         const storedList = localStorage.getItem('list')
         if (storedList) {
             setList(JSON.parse(storedList))
-            }
-            console.log(storedList);
-            }, [])
-           
+        }
+        console.log(storedList);
+    }, [])
+
 
 
     return (
         <div className='bg-[#FFEDFA] h-screen '>
             <div className='container py-8 flex-col flex-wrap h-full justify-self-center w-1/2 '>
-                <div className='bg-[#4F959D] h-screen w-auto rounded-2xl pt-2.5'>
+                <div className='bg-[#4F959D] h-auto w-auto rounded-2xl pt-2.5'>
                     <h2 className='text-4xl text-white mt-5 ml-5 '>👋Getting Started</h2>
                     <div className='h-auto w-auto' >
                         <input type="text" placeholder="Enter your todo......" ref={inputBox} value={data} onChange={(e) => setData(e.target.value)} className="bg-black p-2 mt-10 ml-9 w-11/14 h-10 shadow-black shadow-2xl text-white rounded-2xl border-none outline-none placeholder:text-gray-100 focus:ring-2 focus:ring-green-600" />
-                        <button className="bg-yellow-400 text-white ml-4 p-2 mt-8 rounded-2xl border-none outline-none hover:bg-yellow-500 " onClick={handleBtn} style={{display:isClicked?"none":"inline"}}>Add-To</button>
+                        <button className="bg-yellow-400 text-white ml-4 p-2 mt-8 rounded-2xl border-none outline-none hover:bg-yellow-500 " onClick={handleBtn} style={{ display: isClicked ? "none" : "inline" }}>Add-To</button>  <button className="bg-indigo-400 text-white ml-3 h-10 w-20 mt-8 rounded-2xl border-none outline-none hover:bg-indigo-500 " onClick={() => handleUpdate()}>Update</button> :
                         <div className=' content-div mt-8 flex-col justify-self-start w-11/12'>
                             <h1 style={{ display: list.length ? "block" : "none" }} className='text-4xl text-center text-white italic'>TO-DO LIST </h1>
                             {list.map((item, index) => {
                                 return (
-                                    <List list={list} key={index} item={item} index={index} handleDelete={handleDelete} handleEdit={handleEdit} handleUpdate={handleUpdate} isClicked={isClicked} />
+
+                                    <List key={index} list={list} item={item} index={index} handleDelete={handleDelete} handleEdit={handleEdit} handleUpdate={handleUpdate} isClicked={isClicked} />
+
+
+
                                 )
 
                             })}
